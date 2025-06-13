0057d5d68a24956bc07cb0945074293834c8dad2
2-Eung <eung4256@gmail.com>
2025-06-13 14:42:03 +0900

    interface

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,20 @@
-class Shape {
-    public double area() {
-        return 0;                               // 어짜피 오버라이딩되니까 상관없나???
-    }
+interface Animal {
+    void makeSound();
 }
-class Circle extends Shape {
-    double r;
 
-    public Circle (double r) {
-        this.r = r;
-    }
+class Dog implements Animal {
     @Override
-    public double area() {
-        return Math.PI*r*r;
+    public void makeSound() {
+        System.out.println("멍멍");
     }
 }
-class Rectangle extends Shape {
 
-    double v;
-    double h;
 
-    public Rectangle(double v,double h){
-        this.v = v;
-        this.h = h;
-    }
-    @Override
-    public double area () {
-        return v*h;
-    }
-}
+
 public class Main {
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[2];
+        Animal dog = new Dog();
 
-        shapes[0] = new Circle(3);              // 왜갑자기 바꿔...
-        shapes[1] = new Rectangle(3, 4);
-
-        for (Shape shape : shapes) {
-            System.out.println(shape.area());
-        }
+        dog.makeSound();
     }
 }
